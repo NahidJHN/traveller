83fbb2a3ba03985145afd1b0a1adac1c86c7e91c
NahidJHN <jubaidhossain66@gmail.com>
2022-02-24 22:52:31 +0600

    complete a unresponsive project

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const accordian = document.getElementsByClassName("travel_cross-2");
+const dataItem = document.getElementsByClassName("accordian-data")
+let isShown=false
+for (let i = 0; i < dataItem.length; i++) {
+    accordian[i].addEventListener("click", function () {
+        for (let j = 0; j < accordian.length; j++) {
+            let elemt = dataItem[j]
+            if (elemt.classList.contains("showData")) {
+                elemt.classList.remove("showData")
+                elemt.classList.add("hideData")
+
+            }
+
+        }
+
+        if (!isShown) {
+            dataItem[i].classList.add("showData")
+            dataItem[i].classList.remove("hideData")
+            isShown=true
+        } else {
+            dataItem[i].classList.remove("showData")
+            dataItem[i].classList.add("hideData")
+            isShown=false
+        }
+        
+
+    })
+}
